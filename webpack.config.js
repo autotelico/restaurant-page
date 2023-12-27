075b67b322b1c29ad3b06fce28a44e99ad388a25
Henrique Heron <henriqueheron1@gmail.com>
2023-12-27 14:34:55 -0300

    move template.html to /src

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Restaurant Page',
-      template: './dist/template.html',
+      template: './src/template.html',
     })
   ],
   devtool: 'inline-source-map',
