31c147a132e8af93b9c45eb38279e9d2e77dfa00
Henrique Heron <henriqueheron1@gmail.com>
2023-12-28 09:13:37 -0300

    feat: add background color to active tab

--- src/index.js
+++ src/index.js
@@ -12,17 +12,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const tabs = document.querySelectorAll('[data-tab-target]');
     tabs.forEach(tab => {
-        tab.addEventListener('click', () => {
+
+        tab.addEventListener('click', (e) => {
             // a little loop to remove all other .active classes from other tab contents
+            tabs.forEach(tab => {
+                tab.classList.remove('active');
+                console.log('cleared all .active classes!');
+            })
+            
+            tab.classList.add('active');
+            console.log('Tab got the active class');
+
             const tabContents = document.querySelectorAll('[data-tab-content]');
             tabContents.forEach(tabContent => {
                 tabContent.classList.remove('active');
-                console.log('.active Classes cleared.');
             })
 
             const content = document.querySelector(tab.dataset.tabTarget);
             content.classList.add('active');
-            console.log('the class .active has been added');
         })
     })
 
